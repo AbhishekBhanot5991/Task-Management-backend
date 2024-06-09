be8c028c1bd3443b2bcb56ae4ffd878ec5883494
AbhishekBhanot5991 <abhishekbhanot5991@gmail.com>
2024-06-09 17:00:39 +0530

    updated code

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const userRoutes = require('./routes/userRoutes');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+
 
 app.use(bodyParser.json());
 app.get('/', (req, res) => {
@@ -20,6 +20,8 @@ app.get('/ping', (req, res) => {
 app.use('/api/auth', authRoutes);
 app.use('/api/tasks', taskRoutes);
 app.use('/api/users', userRoutes);
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=>{
     console.log(`Server is running on the port: ${PORT}`)
 })
