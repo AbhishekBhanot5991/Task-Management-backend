0215ed0ef4a42b586b95a71c1703cefcaaf6816e
AbhishekBhanot5991 <abhishekbhanot5991@gmail.com>
2024-06-10 11:11:25 +0530

    updated code

--- config.js
+++ config.js
@@ -7,11 +7,14 @@ console.log("Database:", process.env.DB_NAME);
 console.log("Password:", process.env.DB_PASSWORD ? '****' : 'Not Set');
 console.log("Port:", process.env.DB_PORT);
 const pool = new Pool({
-    user:process.env.DB_USER,
-    host:process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port:process.env.DB_PORT
-})
+    connectionString: process.env.POSTGRES_URL,
+  })
+// const pool = new Pool({
+//     user:process.env.DB_USER,
+//     host:process.env.DB_HOST,
+//     database: process.env.DB_NAME,
+//     password: process.env.DB_PASSWORD,
+//     port:process.env.DB_PORT
+// })
 
 module.exports = pool;
