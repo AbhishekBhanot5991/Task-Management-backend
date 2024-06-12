ed065920350e7921fdd7aff92141ded4f8e87c44
AbhishekBhanot5991 <abhishekbhanot5991@gmail.com>
2024-06-12 11:23:37 +0530

    updated code

--- config.js
+++ config.js
@@ -12,11 +12,11 @@ const pool = new Pool({
     connectionString: process.env.POSTGRES_URL,
     secret:process.env.JWT_SECRET,
 
-    // user:process.env.DB_USER,
-    // host:process.env.DB_HOST,
-    // database: process.env.DB_NAME,
-    // password: process.env.DB_PASSWORD,
-    // port:process.env.DB_PORT,
+    user:process.env.DB_USER,
+    host:process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port:process.env.DB_PORT,
     ssl: {
         rejectUnauthorized: true // If using self-signed certificates
       }
